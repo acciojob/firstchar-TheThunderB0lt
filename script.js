9fdaa230a53908ab5ead461afde35d5cd810c8f8
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-30 22:56:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
   // your code here
+	for (let i = 0; i < bound; i++) {
+		if (text[i] != ' ') {
+			console.log(text[i]);
+			break;
+		}
+	}
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
